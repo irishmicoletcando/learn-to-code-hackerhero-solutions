7d881f15e35d9d05e1b799e7be1521bc474835f7
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-02 23:09:27 +0800

    Initial commit

---
+++ 9 - Print the array.js
@@ -0,0 +1,11 @@
+// Given an array, write a function that prints/logs each number in the array.
+
+function printArray(arr) {
+  for ( let number of arr ) {
+    console.log(number);
+  }
+}
+
+printArray([1,3,5])
+printArray([-1,3,-5])
+printArray([1,2,3,4,5])
