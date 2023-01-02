a078d443acdfc2935e41f315116458eaf3e1eb68
Irish Micole Cando <irishmicolecando@gmail.com>
2023-01-02 20:02:55 +0800

    Initial commit

---
+++ 5 - Countdown by Fives.js
@@ -0,0 +1,7 @@
+// Log positive numbers starting at 50, counting down by fives (exclude 0).
+
+function countdownByFives() {
+  for ( let num = 50; num > 0; num-=5 ) {
+      console.log(num)
+  }
+}
